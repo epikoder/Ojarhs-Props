acfc516718875020e4bd9a90c6e3a0ddbfe2ad82
Ezomon Glory <ezomonglory01@gmail.com>
2022-10-12 13:23:18 +0100

    countdown

--- components/user/Countdown.tsx
+++ components/user/Countdown.tsx
@@ -1,52 +1,75 @@
 import React, { useEffect, useState } from "react";
 
-function CountDownTimer({date}) {
-    const [time, setTime] = useState("")    
-    const {year, month, day} = date    
-    const data = ["Rent", "Electricity Service", "Garden Service"]
-    var randomData = data[Math.floor(Math.random()*data.length)]
-	var date_future = new Date(year, month, day)
+function CountDownTimer({ data }) {
     
+	const [time, setTime] = useState([]);	
 
-    useEffect(() => {        
-        setInterval(()=> {
-           setTime(getTime())
-        }, 1000)
-
-
+    let d = new Date(data.created_at)
+    let date = new Date(d.setMonth(d.getMonth() + data.duration))  
+    const year = date.getFullYear().toString()
+    const month = date.getMonth().toString()
+    const day = date.getDate().toString()      
+        
+    let date_future = new Date (`${year}/${month}/${day}`)
+    
+	useEffect(() => {
+		setInterval(() => {
+			setTime(getTime());
+		}, 1000);
 	}, []);
 
-	useEffect(() => {        
-        setInterval(()=> {
-           setTime(getTime())
-        }, 60000)
+	useEffect(() => {
+		setInterval(() => {
+			setTime(getTime());
+		}, 60000);
 	});
 
-    function getTime() {
-        var date_now = new Date().getTime(); // get current time
+	function getTime() {
+		var date_now = new Date().getTime(); // get current time
 
-        // get total seconds between the times
-        var delta = Math.abs(date_future.getTime() - date_now) / 1000;
+		// get total seconds between the times
+		var delta = Math.abs(date_future.getTime() - date_now) / 1000;
 
-        // calculate (and subtract) whole days
-        var days = Math.floor(delta / 86400);
-        delta -= days * 86400;
+		// calculate (and subtract) whole days
+		var days = Math.floor(delta / 86400);
+		delta -= days * 86400;
 
-        // calculate (and subtract) whole hours
-        var hours = Math.floor(delta / 3600) % 24;
-        delta -= hours * 3600;
+		// calculate (and subtract) whole hours
+		var hours = Math.floor(delta / 3600) % 24;
+		delta -= hours * 3600;
 
-        // calculate (and subtract) whole minutes
-        var minutes = Math.floor(delta / 60) % 60;
-        delta -= minutes * 60;
+		// calculate (and subtract) whole minutes
+		var minutes = Math.floor(delta / 60) % 60;
+		delta -= minutes * 60;
 
-        // what's left is seconds
-        var seconds = delta % 60; // in theory the modulus is not required
+		// what's left is seconds
+		var seconds = Math.floor(delta % 60); // in theory the modulus is not required
 
-        return `  Your ${randomData} Expires in ${days} days, ${hours} hours, ${minutes} minutes`;
-    }   
+		return [days, hours, minutes, seconds];
+	}
 
-	return <div>{time}</div>;
+	return (
+		<div className="flex flex-col space-y-1 justify-center ">
+        	{/*Your Shop Ojarh.com Plaza Shop A2 Expires at  */}
+            <h1>Your {data.type} {data.name} Expires in:</h1>
+			<div className="flex space-x-2">
+            <div className="flex flex-col items-center justify-center"> 
+                <span className="text-[.6rem] text-left  md:text-xs text-gray-400">Days</span>
+                <div className="bold text-[1rem] md:text-[1.5rem] ">{time[0]} : </div>
+            </div>
+
+            <div className="flex flex-col items-center justify-center"> 
+                <span className="text-[.6rem] text-left  md:text-xs text-gray-400">Hrs</span>
+                <div className="bold text-[1rem] md:text-[1.5rem] ">{time[1]} : </div>
+            </div>
+
+            <div className="flex flex-col items-center justify-center"> 
+                <span className="text-[.6rem] text-left  md:text-xs text-gray-400">Mins</span>
+                <div className="bold text-[1rem] md:text-[1.5rem] ">{time[2]} </div>
+            </div>           
+            </div>
+		</div>
+	);
 }
 
 export default CountDownTimer;
--- components/user/UserInfo.tsx
+++ components/user/UserInfo.tsx
@@ -4,7 +4,10 @@ import { resolveFilePath } from "../../helpers/helpers";
 import { User } from "../../Typing.d";
 import CountDownTimer from "./Countdown";
 
-export const UserInfo = ({ user }: { user: User }) => {
+import React from "react";
+import RandomData from "./RandomData";
+
+export const UserInfo = ({ user }: { user: User }) => {   
     const router = useRouter()
     return <>
         <div className="flex flex-row items-start">
@@ -27,7 +30,11 @@ export const UserInfo = ({ user }: { user: User }) => {
                 <div className="text-sm font-semibold text-ellipsis overflow-hidden whitespace-nowrap w-36 md:w-60 lg:w-[40vh] my-2">
                     {user.address}
                 </div>
+                <div>
+                  <RandomData />
+                </div>
             </div>
         </div>
     </>
+
 }
